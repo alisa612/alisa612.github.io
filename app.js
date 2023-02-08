6669ee401fec05d45e1ea51237074c85378c3d7e
AlisaH612 <47835653+AlisaH612@users.noreply.github.com>
2023-02-07 17:45:14 -0800

    test2

---
+++ app.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.style.backgroundColor = "purple";
+  button.style.color = "white";
+  button.style.padding = "10px 20px";
+  button.style.borderRadius = "5px";
+  button.style.fontSize = "18px";
+});
