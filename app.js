d563cbc79037e8b5e58becc36340ef02df9e88f5
AlisaH612 <47835653+AlisaH612@users.noreply.github.com>
2023-02-26 14:54:51 -0800

    msg4

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://xinfangwu:UwcpHfHsbltkQPEA@cluster0.rrs7eyk.mongodb.net/?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+client.connect().then(() => {
+  console.log("Connected to MongoDB");
+  // Perform database operations here
+}).catch(err => {
+  console.error("Failed to connect to MongoDB", err);
+});
+
 let nameInput = document.getElementById("nameInput");
 let continueButton = document.getElementById("continueButton");
 
