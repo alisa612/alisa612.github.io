4eb164cc940560424db9f0d3d6f7d14e04abb882
AlisaH612 <47835653+AlisaH612@users.noreply.github.com>
2023-02-26 15:12:02 -0800

    msg6

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const uri = "mongodb+srv://xinfangwu:UwcpHfHsbltkQPEA@cluster0.rrs7eyk.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(uri);
 
@@ -12,16 +11,16 @@ client.connect().then(() => {
   console.error("Failed to connect to MongoDB", err);
 });
 
-const newUser = { name: nameInput};
-client.db().collection("users").insertOne(newUser, (err, result) => {
-  if (err) {
-    console.error("Failed to insert user", err);
-  } else {
-    console.log("Inserted user", newUser);
-  }
-});
-
 continueButton.addEventListener("click", function() {
   let name = nameInput.value;
   alert("Hello, " + name + "! Welcome to our website.");
+
+  const newUser = { name: nameInput.value};
+  client.db().collection("users").insertOne(newUser, (err, result) => {
+    if (err) {
+      console.error("Failed to insert user", err);
+    } else {
+      console.log("Inserted user", newUser);
+    }
+  });
 });
