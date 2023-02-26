ac4b8b433cafb2cdc5d621f53d0dfd7a995ed820
AlisaH612 <47835653+AlisaH612@users.noreply.github.com>
2023-02-26 15:00:00 -0800

    msg5

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://xinfangwu:UwcpHfHsbltkQPEA@cluster0.rrs7eyk.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
+let nameInput = document.getElementById("nameInput");
+let continueButton = document.getElementById("continueButton");
+
 client.connect().then(() => {
   console.log("Connected to MongoDB");
   // Perform database operations here
@@ -9,8 +12,14 @@ client.connect().then(() => {
   console.error("Failed to connect to MongoDB", err);
 });
 
-let nameInput = document.getElementById("nameInput");
-let continueButton = document.getElementById("continueButton");
+const newUser = { name: nameInput};
+client.db().collection("users").insertOne(newUser, (err, result) => {
+  if (err) {
+    console.error("Failed to insert user", err);
+  } else {
+    console.log("Inserted user", newUser);
+  }
+});
 
 continueButton.addEventListener("click", function() {
   let name = nameInput.value;
