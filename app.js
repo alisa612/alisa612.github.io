e2c7eae25d7381d8b6ed6dc262f8cae300fa95b8
AlisaH612 <47835653+AlisaH612@users.noreply.github.com>
2023-02-26 14:14:43 -0800

    msg2

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-let name = prompt("Please enter your name:");
+let input = document.createElement("input");
+input.type = "text";
+input.placeholder = "Enter your name";
+document.body.appendChild(input);
 
 let button = document.createElement("button");
 button.innerHTML = "Continue";
 document.body.appendChild(button);
 
 button.addEventListener("click", function() {
+  let name = input.value;
   alert("Hello, " + name + "! Welcome to our website.");
 });
