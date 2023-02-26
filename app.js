72bf18b2916984aee600f21ec42230402ec112c9
AlisaH612 <47835653+AlisaH612@users.noreply.github.com>
2023-02-26 14:37:35 -0800

    msg3

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-let input = document.createElement("input");
-input.type = "text";
-input.placeholder = "Enter your name";
-document.body.appendChild(input);
+let nameInput = document.getElementById("nameInput");
+let continueButton = document.getElementById("continueButton");
 
-let button = document.createElement("button");
-button.innerHTML = "Continue";
-document.body.appendChild(button);
-
-button.addEventListener("click", function() {
-  let name = input.value;
+continueButton.addEventListener("click", function() {
+  let name = nameInput.value;
   alert("Hello, " + name + "! Welcome to our website.");
 });
